73262221c3b4b03c616c424047e412087c259e41
Chaitanya <chaitanyagupta172004@gmail.com>
2025-04-11 16:45:20 +0000

    feat: refactor wallet handling to read from dynamic path and update NFT metadata and image details

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -17,29 +17,32 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        const image = "https://gateway.irys.xyz/HdKTJ7AVhdhb4qDWdam4GVaW5M4o9M8hhgLgaLUPGd9L"
+        const metadata = {
+            name: "xud-gye-guru",
+            symbol: "MONKE",
+            description: "xud gye guru",
+            image,
+            attributes: [
+                { trait_type: 'mood', value: 'shocked' }
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your metadata URI: ", myUri);
     }
-    catch(error) {
+    catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
+
+// https://arweave.net/4rLQgu6s1vs3KRgywEVDEx936JTQRQAasT3SvSmXBM3Q
+// https://gateway.irys.xyz/4rLQgu6s1vs3KRgywEVDEx936JTQRQAasT3SvSmXBM3Q
